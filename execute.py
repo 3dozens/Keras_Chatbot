dee062ff2d434bf4fa28e59c0c305c75c06f5284
kento_hayasaka <thunders.1028@gmail.com>
2018-06-05 02:24:00 +0900

    add Embedding

--- execute.py
+++ execute.py
@@ -1,7 +1,7 @@
 from comet_ml import Experiment
 from keras.backend import set_session
 from keras.models import Model
-from keras.layers import Input, LSTM, CuDNNLSTM, Dense, Masking, TimeDistributed, Bidirectional
+from keras.layers import Input, LSTM, CuDNNLSTM, Dense, Masking, TimeDistributed, Bidirectional, Embedding
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
@@ -17,11 +17,12 @@ import numpy as np
 import tbvaccine
 tbvaccine.add_hook(isolate=True, show_vars=False)
 
-mode = "train" # "train" or "inference"
+mode = "inference" # "train" or "inference"
 
-WEIGHTS_FILE = "weights/iranaiyo11.h5"
+WEIGHTS_FILE = "weights/iranaiyo12.h5"
+PRETRAINED_VEC_FILE = "data_set/GloVe/glove.6B.300d.txt"
 tensorboard_logdir = None #"tfboard_logs/weights_512_s_10000_w_10000_l2_2nd"
-SAVE_WEIGHTS = True
+SAVE_WEIGHTS = False
 ONLY_CPU = True
 
 if mode == "train" and SAVE_WEIGHTS and exists(WEIGHTS_FILE):
@@ -39,11 +40,12 @@ if ONLY_CPU:
 START = "startsentence"
 END   = "endsentence"
 
-hidden_size = 512
-batch_size  = 1
-max_epochs  = 300
-num_samples = 20 # None = use all samples
-num_words   = 10000
+hidden_size   = 512
+batch_size    = 5
+max_epochs    = 300
+num_samples   = 30 # None = use all samples
+num_words     = 10000
+embedding_dim = 300
 
 with open("./data_set/cornell movie-dialogs corpus/train.enc") as ftrain_enc:
     train_enc_input = ftrain_enc.readlines()[:num_samples]
@@ -71,18 +73,36 @@ train_dec_input  = np.array(pad_sequences(train_dec_input,  padding="post"))
 train_dec_target = np.array(pad_sequences(train_dec_target, padding="post"))
 
 # データの次元をLSTMレイヤーの入力次元を合わせる。(batch_size, timesteps) => (batch_size, timesteps, feature_dim)
-train_enc_input  = train_enc_input.reshape(train_enc_input.shape[0], train_enc_input.shape[1], 1)
-train_dec_input  = train_dec_input.reshape(train_dec_input.shape[0], train_dec_input.shape[1], 1)
-train_dec_target = train_dec_target.reshape(train_dec_target.shape[0], train_dec_target.shape[1], 1)
+# Embedding Layerを使う場合は内部で3次元に変換されるため必要ない
+# train_enc_input  = train_enc_input.reshape(train_enc_input.shape[0], train_enc_input.shape[1], 1)
+# train_dec_input  = train_dec_input.reshape(train_dec_input.shape[0], train_dec_input.shape[1], 1)
+# train_dec_target = train_dec_target.reshape(train_dec_target.shape[0], train_dec_target.shape[1], 1)
+
+# Embedding Matrixをつくる
+embedding_index = {}
+with open(PRETRAINED_VEC_FILE, "r") as f:
+    for line in f:
+        _line = line.split()
+        word, vec = _line[0], np.asarray(_line[1:])
+        embedding_index[word] = vec
+
+embedding_matrix = np.zeros((len(tokenizer.word_index) + 1, 300))
+for word, i in tokenizer.word_index.items():
+    embedding_vector = embedding_index.get(word)
+    if embedding_vector is not None:
+        embedding_matrix[i] = embedding_vector
 
 # layer declaration
-encoder_inputs = Input(shape=(train_enc_input.shape[1], train_enc_input.shape[2])) # (timestep, feature_dim)
-decoder_inputs = Input(shape=(train_dec_input.shape[1], train_dec_input.shape[2]))
+# encoder_inputs = Input(shape=(train_enc_input.shape[1], train_enc_input.shape[2])) # (timestep, feature_dim)
+# decoder_inputs = Input(shape=(train_dec_input.shape[1], train_dec_input.shape[2]))
+encoder_inputs = Input(shape=(train_enc_input.shape[1],))
+decoder_inputs = Input(shape=(train_dec_input.shape[1],))
+embedding      = Embedding(len(tokenizer.word_index) + 1, embedding_dim, weights=[embedding_matrix], trainable=False)
 encoder_lstm   = CuDNNLSTM(hidden_size, go_backwards=True, return_sequences=True, name="encoder_lstm")
 encoder_lstm2  = CuDNNLSTM(hidden_size, return_state=True, name="encoder_lstm2")
 decoder_lstm   = CuDNNLSTM(hidden_size, return_sequences=True, name="decoder_lstm") # LSTM = 382s 42ms/step CuDNNLSTM = 107s 12ms/step
 decoder_lstm2  = CuDNNLSTM(hidden_size, return_sequences=True, name="decoder_lstm2")
-decoder_dense  = Dense(num_words, activation="softmax", name="output_dence")
+decoder_dense  = Dense(num_words, activation="linear", name="output_dence")
 if ONLY_CPU:
     encoder_lstm  = LSTM(hidden_size, go_backwards=True, return_state=True, return_sequences=True, name="encoder_lstm")
     encoder_lstm2 = LSTM(hidden_size, return_state=True, name="encoder_lstm2")
@@ -93,23 +113,33 @@ def train():
     # if SAVE_WEIGHTS:
     #     experiment = Experiment(api_key=environ["COMET_ML_API_KEY"])
 
-    layer1_out          = encoder_lstm(encoder_inputs)
-    _, state_h, state_c = encoder_lstm2(layer1_out)
+    enc_emb    = embedding(encoder_inputs)
+    layer1_out = encoder_lstm(enc_emb)
+    _, h, c    = encoder_lstm2(layer1_out)
+    encoder_states = [h, c]
 
-    encoder_states = [state_h, state_c]
+    dec_emb        = embedding(decoder_inputs)
+    output1        = decoder_lstm(dec_emb, initial_state=encoder_states)
+    output2        = decoder_lstm2(output1)
+    decoder_output = decoder_dense(output2)
 
-    outputs1        = decoder_lstm(decoder_inputs, initial_state=encoder_states)
-    outputs2        = decoder_lstm2(outputs1)
-    decoder_outputs = decoder_dense(outputs2)
+    model = Model([encoder_inputs, decoder_inputs], decoder_output)
 
-    model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
     # loss = partial(sparse_categorical_crossentropy, from_logits=True)
-    loss = "sparse_categorical_crossentropy"
+    # loss = "sparse_categorical_crossentropy"
+    # workaround of https://github.com/tensorflow/tensorflow/issues/17150
+    def sparse_loss(y_true, y_pred):
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred)
+        loss_mean = tf.reduce_mean(loss)
+        return loss_mean
+    loss = sparse_loss
+
     callbacks = [EarlyStopping(monitor="loss", patience=5, verbose=1)]
     if tensorboard_logdir is not None:
         callbacks.append(TensorBoard(log_dir=tensorboard_logdir, histogram_freq=1))
 
-    model.compile(optimizer="adam", loss=loss)
+    decoder_target = tf.placeholder(dtype="int32", shape=(None, train_dec_target.shape[1]))
+    model.compile(optimizer="adam", loss=loss, target_tensors=[decoder_target])
     model.fit([train_enc_input, train_dec_input], train_dec_target,
               batch_size=batch_size,
               epochs=max_epochs,
@@ -122,19 +152,21 @@ def train():
         model.save_weights(WEIGHTS_FILE)
 
 def inference():
-    encoder_inputs = Input(shape=(None, 1))
+    encoder_inputs = Input(shape=(None,))
 
-    layer1_out = encoder_lstm(encoder_inputs)
+    enc_emb    = embedding(encoder_inputs)
+    layer1_out = encoder_lstm(enc_emb)
     _, h, c    = encoder_lstm2(layer1_out)
 
     encoder_states = [h, c]
 
     encoder_model = Model(encoder_inputs, encoder_states)
+    #print(encoder_model.summary()); exit()
     encoder_model.load_weights(WEIGHTS_FILE, by_name=True)
 
     # 学習時はencoder_statesをそのまま入力するだけだったのでstateのInputは不要だったが、
     # 推論時は生成の各ステップでdecoderのstateを入力するので、Inputが必要。
-    decoder_inputs = Input(shape=(1,1))
+    decoder_inputs = Input(shape=(1,))
     state_input_h1 = Input(shape=(hidden_size,))
     state_input_c1 = Input(shape=(hidden_size,))
     state_input_h2 = Input(shape=(hidden_size,))
@@ -145,14 +177,17 @@ def inference():
     decoder_lstm.return_state  = True
     decoder_lstm2.return_state = True
 
-    decoder_out,  h1, c1 = decoder_lstm(decoder_inputs, initial_state=states_inputs[:2])
+    dec_emb              = embedding(decoder_inputs)
+    decoder_out,  h1, c1 = decoder_lstm(dec_emb, initial_state=states_inputs[:2])
     decoder_out2, h2, c2 = decoder_lstm2(decoder_out,  initial_state=states_inputs[2:])
-    decoder_outputs       = decoder_dense(lstm_outputs2)
+    decoder_output       = decoder_dense(decoder_out2)
     states_outputs = [h1, c1, h2, c2]
-    decoder_model  = Model([decoder_inputs] + states_inputs, [decoder_outputs] + states_outputs)
+    decoder_model  = Model([decoder_inputs] + states_inputs, [decoder_output] + states_outputs)
     decoder_model.load_weights(WEIGHTS_FILE, by_name=True)
 
-    input_seq = tokenizer.texts_to_sequences(["You have my word.  As a gentleman"])
+    # input_seq = tokenizer.texts_to_sequences(["You have my word.  As a gentleman"])
+    input_seq = tokenizer.texts_to_sequences(["The thing is, Cameron -- I'm at the mercy of a particularly hideous breed of loser."])
+    # input_seq = tokenizer.texts_to_sequences(["Why?"])
     input_seq = np.array(input_seq)
     # input_seq = input_seq[:,:,np.newaxis]
 
@@ -166,10 +201,12 @@ def inference():
     input_seqs = [input_seq]
 
     results = []
+    """--- INFERENCE PROCESS ---"""
     """ encode """
     for i, input_seq in enumerate(input_seqs):
         print(i)
-        states_values = encoder_model.predict(input_seq[:,:,np.newaxis]) # (timesteps, feature_dim) => (batch_size, timesteps, feature_dim)
+        # print(input_seq.shape); exit()
+        states_values = encoder_model.predict(input_seq) # (timesteps, feature_dim) => (batch_size, timesteps, feature_dim)
         # 1st layer is initialized by encoder states
         # 2nd layer is initialized by zeros
         # Input expects (batch_size, hidden_size)
@@ -178,7 +215,7 @@ def inference():
         second_layer_init_states = [np.zeros((1,hidden_size)), np.zeros((1,hidden_size))]
         states_values            = states_values + second_layer_init_states
 
-        target_seq = np.array([[[tokenizer.word_index[START]]]]) # (1, 1, 1)
+        target_seq = np.array([[tokenizer.word_index[START]]]) # (1, 1)
 
         index_to_word      = {v:k for k,v in tokenizer.word_index.items()}
         index_to_word[0]   = ""
@@ -186,29 +223,28 @@ def inference():
         MAX_GEN_WORD       = 1000
         """ decode """
         for j in range(MAX_GEN_WORD):
-            output_tokens, h1, c1, h2, c2 = decoder_model.predict([target_seq] + states_values)
+            output_token, h1, c1, h2, c2 = decoder_model.predict([target_seq] + states_values)
 
-            sampled_index = np.argmax(output_tokens[0, 0, :])
+            sampled_index = np.argmax(output_token[0, 0, :])
             sampled_word = index_to_word[sampled_index]
+            print_debug(j, output_token, sampled_index, sampled_word, generated_sentense)
             if (sampled_word == END):
                 break
 
-            print_debug(j, output_tokens, sampled_index, sampled_word, generated_sentense)
             generated_sentense += str(sampled_word) + " "
 
-
-            target_seq = np.array([[[sampled_index]]])
+            target_seq = np.array([[sampled_index]])
             states_values = [h1, c1, h2, c2]
 
         results.append(generated_sentense)
 
     return results
 
-def print_debug(count, output_tokens, sampled_index, sampled_word, decoded_sentense):
+def print_debug(count, output_token, sampled_index, sampled_word, decoded_sentense):
     print("-----")
     print("count = " + str(count))
     print("raw outputs = ")
-    print(output_tokens)
+    print(output_token)
     print("index = ")
     print(sampled_index)
     print("word = ")
